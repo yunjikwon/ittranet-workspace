03dbccfbd1aca1d8a1b7185e7d45b38a6e48f34d
Sunmin Lee <bitbeatbbi@naver.com>
2022-02-09 15:58:11 +0900

    회원가입테스트_선민
    Test : 회원가입 테스트 페이지 추가
    author : 이선민
    
    회원 가입 테스트 페이지
    회원 가입 기능 완성된 후 링크 삭제
    실제 회원 가입은 메일로 해당 주소 url링크를 보내주는 방향으로 완성시켜야 함
    
    - delete beapo.txt
    - add views/member/joinForm.jsp
    - update com.h4j.ITranet.employee.controller.EmployeeController

--- ittranetProject/src/main/java/com/h4j/ITtranet/employee/controller/EmployeeController.java
+++ ittranetProject/src/main/java/com/h4j/ITtranet/employee/controller/EmployeeController.java
@@ -16,6 +16,7 @@ public class EmployeeController {
 	@Autowired
 	private EmployeeService eService;
 	
+	// 로그인
 	@RequestMapping("hi.me")
 	public ModelAndView loginMember(Employee e, HttpSession session, ModelAndView mv) {
 		
@@ -31,10 +32,26 @@ public class EmployeeController {
 		return mv;
 	}
 	
+	// 로그아웃
 	@RequestMapping("bye.me")
 	public String logoutMember(HttpSession session) {
 		session.invalidate();
 		return "redirect:/";
+	}
+	
+	// 회원가입페이지 호출(메일 => 링크 클릭 => 회원가입 페이지(당연히 서버 돌고 있어야 함)
+	// 링크 주소 : http://localhost:8174/ITtranet/joinForm.me
+	@RequestMapping("joinForm.me")
+	public String joinForm() {
+		return "member/joinForm";
+	}
+	
+	// 회원가입 요청 => DB에 정보 담김 but 상태(status)는 W(wait)
+	// 관리자가 가입 승인을 하면 status가 Y로 바뀌고 로그인 가능
+	// * 회원가입 요청시에는 status W로 insert
+	@RequestMapping("join.me")
+	public void joinMember(Employee e) {
+		System.out.println(e);
 	}
 	
 
