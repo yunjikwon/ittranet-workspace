45000c0c9d5367813c686c30bd40e400f329b00f
leejaeinn <yourjaein@gmail.com>
2022-02-09 15:40:30 +0900

    프로젝트 클래스 추가 - 재인
    feat : 프로젝트 클래스 추가
    author : 재인

--- ittranetProject/src/main/java/com/h4j/ITtranet/project/model/dao/ProjectDao.java
+++ ittranetProject/src/main/java/com/h4j/ITtranet/project/model/dao/ProjectDao.java
@@ -1,5 +1,14 @@
 package com.h4j.ITtranet.project.model.dao;
 
+import java.util.ArrayList;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.h4j.ITtranet.project.model.vo.Project;
+
+@Repository
 public class ProjectDao {
+	
 
 }
--- ittranetProject/src/main/java/com/h4j/ITtranet/project/model/service/ProjectService.java
+++ ittranetProject/src/main/java/com/h4j/ITtranet/project/model/service/ProjectService.java
@@ -1,5 +1,18 @@
 package com.h4j.ITtranet.project.model.service;
 
+import java.util.ArrayList;
+
+import com.h4j.ITtranet.project.model.vo.Project;
+
 public interface ProjectService {
 
+	// 프로젝트 생성
+	int insertProject(Project p);
+	
+	// 프로젝트 리스트 조회
+	ArrayList<Project> selectProject(int projectNo);
+	
+	// 프로젝트 수정
+	int updateProject(Project p);
+	
 }
