88c1305ea68109eb64b08dd909fdb36b97a6475f
yunjikwon <yungee92@gmail.com>
2022-02-18 09:58:03 +0900

    공지사항 게시판 구현
    docs:
     - config문서 내 notice 경로 수정
     - notice-mapper.xml 업데이트
     - 공지사항 전체보기 및 페이징 처리
     - 공지사항 상세보기
     - 공지사항 수정하기
     - 공지사항 삭제하기
     - 댓글 및 대댓글 등록 / 수정 / 삭제
    author : 권윤지

---
+++ ittranetProject/src/main/java/com/h4j/ITtranet/notice/model/vo/NoticeHeader.java
@@ -0,0 +1,17 @@
+package com.h4j.ITtranet.notice.model.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@Setter
+@Getter
+@ToString
+@AllArgsConstructor
+public class NoticeHeader {
+	private int noticeNo;
+	private String noticeHeader;
+}
